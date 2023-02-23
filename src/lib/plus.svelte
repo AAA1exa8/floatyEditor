f2f0d5fe1eb8000cbde022071d9d191e07101e26
AAA1exa8 <alexandrpleskot@gmail.com>
2023-02-23 19:33:27 +0100

    cleanup

--- src/lib/plus.svelte
+++ src/lib/plus.svelte
@@ -1,7 +1,7 @@
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-<span class="absolute right-0 text-size-40px cursor-pointer select-none" style="font-family: material_icons;" on:click={addEditor}>
+<span class="w-69px h-69px text-center absolute right-10px top-10px text-size-60px cursor-pointer select-none z-999 rounded-full font-$material_icons" style="box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);" on:click={addEditor}>
     &#xF4FE;
-</span>    
+</span>  
 
 
 
@@ -18,6 +18,5 @@
             editorCounter = editorCounter.concat({z: Math.max(...editorCounter.map(editor => editor.z))+1, 
                 id:editorCounter[editorCounter.length-1].id+1})
         }
-        console.log(editorCounter)
     }    
 </script>
