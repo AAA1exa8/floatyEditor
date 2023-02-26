fee0082a2959ff922fc5609cc7e480ab35649b9c
AAA1exa8 <alexandrpleskot@gmail.com>
2023-02-26 23:38:30 +0100

    navigation changes (only visual) and refactor to stores

---
+++ src/lib/navbar.svelte
@@ -0,0 +1,42 @@
+<div class="h-15"></div>
+<header class="fixed bg-orange-200 z-2 w-screen -my-15 font-bold">
+    <div>
+        <nav>
+            <span class="text-3xl">
+                floatyEditor
+            </span>
+            <ul>
+                <li>Floaties</li>
+                <li>Notes</li>
+                <li>Blog</li>
+                <li>Share</li>
+                <li>about</li>
+            </ul>
+        </nav>
+    </div>
+</header>
+
+<style>
+    *{
+        font-family: sans-serif;
+    }
+    header{
+        box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+    }
+    nav{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0.5rem 1rem;
+    }
+    ul{
+        display: flex;
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+    li{
+        margin: 0 0.5rem;
+        color: #333;
+    }
+</style>
--- src/lib/plus.svelte
+++ src/lib/plus.svelte
@@ -1,32 +1,91 @@
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-{#if bigWidth}
-    <span class="w-17 h-17 text-center absolute right-2.5 top-2.5 text-3.75rem cursor-pointer select-none z-999 rounded-full font-$material_icons bg-light-50" style="box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);" on:click={addEditor}>
+{#if enoughWidth}
+    <span class="w-17 h-17 text-center absolute right-2.5 top-16 text-3.75rem cursor-pointer select-none z-999 rounded-full font-$material_icons bg-light-50" style="box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);" on:click={addEditor}>
         &#xF4FE;
     </span> 
 {:else}
-    <div class="flex w-full flex justify-center fixed bottom-0 bg-light-50 z-999">
-        <span class="w-25 h-25 text-center text-22 cursor-pointer select-none z-999 rounded-full font-$material_icons bg-light-50 relative bottom-8" style="box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);" on:click={addEditor}>
-            &#xF4FE;
-        </span>
+    <!-- ! finish this is just visual -->
+    <div class="flex w-full h-30 flex justify-center items-center fixed bottom-0 bg-light-50 z-999" style="box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);">
+        <div class="flex items-center justify-center w-full">
+            <span class="selector"
+                on:click={() => editor.chain().focus().toggleHeading({ level: 1}).run()}
+                class:active={editor.isActive('heading', { level: 1 })}
+            >
+                &#xF799;
+            </span>
+            <span class="selector"
+                on:click={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+                class:active={editor.isActive('heading', { level: 2 })}
+            >
+                &#xF79F;
+            </span>
+            <span class="selector" 
+                on:click={() => {editor.chain().focus().setParagraph().run()}} 
+                class:active={editor.isActive('paragraph')}>
+                &#xF4B4;
+            </span>
+        </div>
+        <div class="h-25 w-max text-center cursor-pointer select-none z-999 rounded-full font-$material_icons bg-light-50 " style="box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);" on:click={addEditor}>
+            <span class="text-22">
+                &#xF4FE;
+            </span>
+        </div>
+
+        <div class="flex items-center justify-center w-full">
+            <span class="selector"
+                on:click={() => editor.chain().focus().toggleHeading({ level: 1}).run()}
+                class:active={editor.isActive('heading', { level: 1 })}
+            >
+                &#xF5F0;
+            </span>
+            <span class="selector"
+                on:click={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+                class:active={editor.isActive('heading', { level: 2 })}
+            >
+                &#xF5F4;
+            </span>
+            <span class="selector" 
+                on:click={() => editor.chain().focus().setParagraph().run()} 
+                class:active={editor.isActive('paragraph')}>
+                &#xF2C8;
+            </span>
+        </div>
     </div>
+    <!-- ! this is just visual -->
 {/if}
  
 
 
 
-
+<style lang="postcss">
+    .selector {
+        @apply items-center justify-center text-3xl cursor-pointer select-none z-999 rounded-full bg-light-50 mx-5 relative;
+        font-family: material_icons;
+    }
+</style>
 
 
 
 <script lang="ts">
 	import { onMount } from "svelte";
 
+    import { focusedEditor, bigWidth} from "$lib/stores/stores";
 
-    export var bigWidth: boolean = true;
-
+    
     export var editorCounter= [
         {z: 1, id:0}
     ];    
+
+    var enoughWidth: boolean;
+    var editor: any;
+
+    focusedEditor.subscribe((value) => {
+        editor = value;
+    });
+
+    bigWidth.subscribe((value) => {
+        enoughWidth = value;
+    });
 
     onMount(() => {
         checkWidth();
@@ -35,7 +94,7 @@
     });
 
     function checkWidth(){
-        bigWidth = document.body.clientWidth > 768;
+        bigWidth.set(document.body.clientWidth > 768); 
     }
 
     function addEditor(){
